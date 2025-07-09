8206e7fd4f9309031fd0c1bdfd76dfeeb87ea404
Adrian Tennies <adrian@tennies.us>
2025-07-08 23:24:16 -0500

    adding a Navigation Bar

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-
+	import NavBar from './NavBar.svelte';
+	//import { svelte } from '@sveltejs/vite-plugin-svelte';
 	let { children } = $props();
 </script>
-
+<NavBar />
 {@render children()}
---
+++ myapp/src/routes/NavBar.svelte
@@ -0,0 +1,14 @@
+<script> console.log('erm, what the sigma');</script>
+
+<nav class="bg-gray-800 p-4">
+  <div class="container mx-autob flex items-center justify-between">
+    <a href="/" class="text-white text-lg font-semibold">My Portfolio</a>
+    <div class="space-x-4">
+      <a href="/" class="text-gray-300 hover:text-white"><b>Home</b></a>
+      <a href="/projects" class="text-gray-300 hover:text-white"><b>My Projects</b></a>
+      <a href="/about" class="text-gray-300 hover:text-white"><b>About</b></a>
+      <a href="/contact" class="text-gray-300 hover:text-white"><b>Contact</b></a>
+      <a href="/contact" class="text-gray-300 py-1 px-2 blur-xs rounded-md transition delay-15 duration-600 ease-in-out hover:bg-blue-800 hover:blur-none"><b>Test</b></a>
+    </div>
+  </div>
+</nav>
