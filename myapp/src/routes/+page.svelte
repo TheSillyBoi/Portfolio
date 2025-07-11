54da2f2cc867bb80b5a0795f37dabbee34d47562
Adrian Tennies <adrian@tennies.us>
2025-07-10 20:21:05 -0500

    Adding Content

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<div  class="py-25 px-40">
+    <h1 class="text-[6rem] font-bold">Adrian Tennies</h1>
+<br>
+<br>
+<br>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
           <img src="../favicon.svg" width="58" alt = "Minecraft Logo">
+</div>
--- myapp/src/routes/NavBar.svelte
+++ myapp/src/routes/NavBar.svelte
@@ -1,6 +1,6 @@
-<script> console.log('erm, what the sigma');</script>
+<script></script>
 
-<nav class="bg-blue-900 p-3">
+<nav class="bg-indigo-950 p-3">
   <div class="container mx-autob flex items-center justify-between">
     <a href="/" class="text-white text-lg font-semibold">My Portfolio</a>
     <div class="space-x-2">
@@ -8,6 +8,9 @@
       <a href="/projects" class="text-gray-300 py-2 px-3 rounded-md transition delay-15 duration-600 ease-in-out hover:bg-black"><b>My Projects</b></a>
       <a href="/about"    class="text-gray-300 py-2 px-3 rounded-md transition delay-15 duration-600 ease-in-out hover:bg-black"><b>About</b></a>
       <a href="/contact"  class="text-gray-300 py-2 px-3 rounded-md transition delay-15 duration-600 ease-in-out hover:bg-black"><b>Contact</b></a>
+      <a href="/blog"  class="text-gray-300 py-2 px-3 rounded-md transition delay-15 duration-600 ease-in-out hover:bg-black"><b>Blog</b></a>
+      <a href="/demo/lucia"  class="text-gray-300 py-2 px-3 rounded-md transition delay-15 duration-600 ease-in-out hover:bg-black"><b>Sign In</b></a>
+
     </div>
   </div>
 </nav>
